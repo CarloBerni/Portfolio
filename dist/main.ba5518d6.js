9bd28a6bfb68bd687220f6fdb61d2a88e6820d0f
CarloBerni <carlo.berni@hetic.net>
2019-01-23 12:02:31 +0100

    commit

--- dist/main.ba5518d6.js
+++ dist/main.ba5518d6.js
@@ -490,7 +490,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49494" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52928" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
