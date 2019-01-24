0816fa2276a16bb62a24ad62ec17297cbfa48bd1
CarloBerni <carlo.berni@hetic.net>
2019-01-24 13:02:05 +0100

    finish scroll prettier before design

--- src/Js/main.js
+++ src/Js/main.js
@@ -5,8 +5,7 @@ console.log(helpers.double(12));
 console.log(rellax);
 
 
-var Rellax = new rellax(".rellax", {
-});
+var Rellax = new rellax(".rellax", {});
 
 
 
@@ -15,22 +14,22 @@ var horizontals = document.querySelectorAll('.section--horizontal');
 
 // For each horizontal scroll element, apply the effect
 horizontals.forEach(function (horizontal) {
-  
+
   // Get the inner element
   var inner = horizontal.querySelector('.section__inner');
 
   // When the user scroll and an animation frame is available
-  window.addEventListener('scroll', function() {
-    window.requestAnimationFrame(function() {
-      
+  window.addEventListener('scroll', function () {
+    window.requestAnimationFrame(function () {
+
       // The distance to scroll inside the horizontal element
       // is its height - the window's height
       var toGo = horizontal.offsetHeight - window.innerHeight;
-      
+
       // The scroll position inside the element
       // is the scroll position - the element's distance from the top
       var position = window.scrollY - horizontal.offsetTop;
-      
+
       // The progression between 0 & 1 is the scroll position
       // inside the element divided by the distance to scroll
       var progression = (position / toGo);
@@ -50,7 +49,7 @@ horizontals.forEach(function (horizontal) {
       } else {
         horizontal.classList.remove('section--isScrolled');
       }
-      
+
       // Set the translation for the element
       setTranslateX(inner, progression);
     });
@@ -64,10 +63,10 @@ function setTranslateX(element, progression) {
   } else if (progression < 0) {
     progression = 0;
   }
-  
+
   // The size to move is the element width minus the window width
   var toMove = element.offsetWidth - window.innerWidth;
-  
+
   // The transform factor is the size to move multiplied by the progression
   var transform = (-1 * toMove * progression) + 'px';
   element.style.transform = 'translateX(' + transform + ')';
