1fbb14695408c8b55f748e0822d251f39aacdfdf
CarloBerni <carlo.berni@hetic.net>
2019-01-24 12:49:48 +0100

    finish scroll horizontal

--- dist/main.ba5518d6.js
+++ dist/main.ba5518d6.js
@@ -460,7 +460,62 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 console.log(_sub.default.double(12));
 console.log(_rellax.default);
-var Rellax = new _rellax.default(".rellax", {});
+var Rellax = new _rellax.default(".rellax", {}); // Get all horizontal scroll element
+
+var horizontals = document.querySelectorAll('.section--horizontal'); // For each horizontal scroll element, apply the effect
+
+horizontals.forEach(function (horizontal) {
+  // Get the inner element
+  var inner = horizontal.querySelector('.section__inner'); // When the user scroll and an animation frame is available
+
+  window.addEventListener('scroll', function () {
+    window.requestAnimationFrame(function () {
+      // The distance to scroll inside the horizontal element
+      // is its height - the window's height
+      var toGo = horizontal.offsetHeight - window.innerHeight; // The scroll position inside the element
+      // is the scroll position - the element's distance from the top
+
+      var position = window.scrollY - horizontal.offsetTop; // The progression between 0 & 1 is the scroll position
+      // inside the element divided by the distance to scroll
+
+      var progression = position / toGo; // If progression is between 0 & 1 that means we are viewing
+      // the section so fix it
+
+      if (progression > 0 && progression < 1) {
+        horizontal.classList.add('section--isFixed');
+      } else {
+        // Don't fix it
+        horizontal.classList.remove('section--isFixed');
+      } // If the progression is above 1 that means the
+      // section has been completly scrolled
+
+
+      if (progression >= 1) {
+        horizontal.classList.add('section--isScrolled');
+      } else {
+        horizontal.classList.remove('section--isScrolled');
+      } // Set the translation for the element
+
+
+      setTranslateX(inner, progression);
+    });
+  });
+});
+
+function setTranslateX(element, progression) {
+  // Limit the progression factor between 0 & 1
+  if (progression > 1) {
+    progression = 1;
+  } else if (progression < 0) {
+    progression = 0;
+  } // The size to move is the element width minus the window width
+
+
+  var toMove = element.offsetWidth - window.innerWidth; // The transform factor is the size to move multiplied by the progression
+
+  var transform = -1 * toMove * progression + 'px';
+  element.style.transform = 'translateX(' + transform + ')';
+}
 },{"rellax":"../node_modules/rellax/rellax.js","./sub":"Js/sub.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -488,7 +543,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52046" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52413" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
