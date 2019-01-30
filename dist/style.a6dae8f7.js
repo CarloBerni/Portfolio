856fd261a0fac7a3abe75b0420ab3f6619c349a9
CarloBerni <carlo.berni@hetic.net>
2019-01-30 11:21:40 +0100

    bootstrap finish

--- dist/style.a6dae8f7.js
+++ dist/style.a6dae8f7.js
@@ -176,7 +176,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.eot":[["glyphicons-halflings-regular.967cec0a.eot","../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.eot"],"../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.eot"],"./../../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff2":[["glyphicons-halflings-regular.f2b3732e.woff2","../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff2"],"../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff2"],"./../../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff":[["glyphicons-halflings-regular.36b461ff.woff","../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff"],"../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff"],"./../../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.ttf":[["glyphicons-halflings-regular.558ad4e8.ttf","../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.ttf"],"../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.ttf"],"./../../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.svg":[["glyphicons-halflings-regular.2f8f65a1.svg","../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.svg"],"../node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51202" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51370" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
