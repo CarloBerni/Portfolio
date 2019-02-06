22fda16294623fbd1d65a1553248722bfe4e3e15
CarloBerni <carlo.berni@hetic.net>
2019-02-06 15:08:59 +0100

    last

--- dist/style.a6dae8f7.js
+++ dist/style.a6dae8f7.js
@@ -176,7 +176,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../img/paper.jpg":[["paper.899f625e.jpg","img/paper.jpg"],"img/paper.jpg"],"./../img/sam-mgrdichian-390682-unsplash-min.jpg":[["sam-mgrdichian-390682-unsplash-min.ac8be3ea.jpg","img/sam-mgrdichian-390682-unsplash-min.jpg"],"img/sam-mgrdichian-390682-unsplash-min.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../img/paper.jpg":[["paper.899f625e.jpg","img/paper.jpg"],"img/paper.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49690" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49903" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
