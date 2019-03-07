d759002e206190e9e8caab97d407dbdd5da57634
CarloBerni <carlo.berni@hetic.net>
2019-03-07 11:00:25 +0100

    something bold ?

--- dist/main.ba5518d6.js
+++ dist/main.ba5518d6.js
@@ -543,7 +543,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50468" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50486" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
