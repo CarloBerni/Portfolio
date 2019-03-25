056d09c460964eef93859e683cc0b2cf089c3c2d
CarloBerni <carlo.berni@hetic.net>
2019-03-25 12:06:09 +0100

    add font kanit

--- dist/main.ba5518d6.js
+++ dist/main.ba5518d6.js
@@ -563,7 +563,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61572" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61852" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
